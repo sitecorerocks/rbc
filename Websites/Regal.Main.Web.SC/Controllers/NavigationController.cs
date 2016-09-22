5170e0a51188d411e15f9115e10948c7a4b1c6e2
sitecorerocks <vrathore9119@gmail.com>
2016-09-23 02:09:31 +0530

    Files added/removed

---
+++ Websites/Regal.Main.Web.SC/Controllers/NavigationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Regal.Main.Web.SC.Controllers
+{
+    public class NavigationController : Controller
+    {
+        // GET: Navigation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
